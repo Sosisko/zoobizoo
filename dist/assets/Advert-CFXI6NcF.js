e6316bacf4096f8b2862aff3e02b867d9a1f2e31
Aleksey <svetlampy@mail.ru>
2024-03-24 16:15:28 +0500

    adding dist subtree

---
+++ dist/assets/Advert-CFXI6NcF.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as r}from"./index-CjWOqsPm.js";const t={};function n(o,s){return r(),c("div",null,"ОБЪЯБЛЕНИЯ")}const a=e(t,[["render",n]]);export{a as default};
--- dist/assets/Login-DMUvjkxp.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as n}from"./index-DJrsQB9Z.js";const o={};function r(t,s){return n(),c("div",null,"LOGIN")}const a=e(o,[["render",r]]);export{a as default};
